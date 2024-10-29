838fcde5bb0ac97a5d36d2271c03246ae9f9041c
Abhishek Aryan <aryan.abhishek1829@gmail.com>
2024-10-29 16:36:38 +0530

    Added http server logic to serve index.html for the root path

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	
+	fileServer := http.FileServer(http.Dir("./static"));
+	http.Handle("/", fileServer)
+	
+	fmt.Println("Starting server on port 8080...")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
