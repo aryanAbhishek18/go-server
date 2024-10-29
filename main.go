c3bee016c42b24a6997f2ef062cb974b37201635
Abhishek Aryan <aryan.abhishek1829@gmail.com>
2024-10-29 16:45:45 +0530

    Added a /hello path

--- main.go
+++ main.go
@@ -6,11 +6,20 @@ import (
 	"net/http"
 )
 
+func helloHandler(res http.ResponseWriter, req *http.Request) {
+	if req.Method != "GET" {
+		http.Error(res, "method not supported", http.StatusNotFound)
+	}
+	fmt.Fprint(res, "Hello user!");
+}
+
 func main() {
 	
 	fileServer := http.FileServer(http.Dir("./static"));
 	http.Handle("/", fileServer)
 	
+	http.HandleFunc("/hello", helloHandler)
+
 	fmt.Println("Starting server on port 8080...")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
